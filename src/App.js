7ece04c5d909f6cee4b7086c2a7f4070af7cc691
shrutiverma2208 <shrutiverma2208@gmail.com>
2024-02-02 22:42:10 +0530

    Added responsive button component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Button from './Components/Button';
+import img1 from './images/img1.avif';
+import logo1 from './images/logo1.jpeg';
 
 function App() {
+   const handleClick=()=>{
+    console.log('clicked');
+   }
   return (
     <div className="App">
-     <Button/>
+     <Button
+     mainImage={img1}
+     iconImage={logo1}
+     mainText='abcd'
+     subText='1234'
+     onClick={handleClick}
+     
+     />
     </div>
   );
 }
--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,29 +1,23 @@
 import React from 'react'
-import img1 from '../images/img1.avif';
-import logo1 from '../images/logo1.jpeg';
+
 import buttonImg from './buttonImg.css'
 
-const Button = () => {
+const Button = ({mainImage,iconImage, mainText, subText, onClick }) => {
   return (
-    <div>
-        <button className='button-full'>
-            <img src={img1} alt='button-img' style={{height:'32vh', width:'36vh', borderRadius:'25px'}}></img>
-            <div className='bottom-button'> 
-                <img src={logo1} style={{height:'40px',width:'40px',borderRadius:'60px'}}></img>
-                <h3 style={{margin:'10px'}}>Title</h3>
-                <h4 style={{margin:'10px'}}>Title 2</h4>
+    <div className='container'>
+        <button className='tiles-full' onClick={onClick}>
+            <img src={mainImage} alt='tiles-img' style={{height:'37vh', width:'39vh', borderRadius:'15px'}}></img>
+            <div className='tiles-bottom'> 
+                <img src={iconImage} style={{height:'4.5vh',width:'4.5vh',borderRadius:'10px'}}></img>
+                <div className='icon-text'>
+                  <h3 style={{margin:'3px',fontSize:'2vw'}}>{mainText}</h3>
+                  <h4 style={{margin:'3px',fontSize:'1.5vw'}}>{subText}</h4>
+                </div>
+               
                 </div>
            
         </button>
-        <button className='button-full'>
-            <img src={img1} alt='button-img' style={{height:'32vh', width:'36vh', borderRadius:'25px'}}></img>
-            <div className='bottom-button'> 
-                <img src={logo1} style={{height:'40px',width:'40px',borderRadius:'60px'}}></img>
-                <h3 style={{margin:'10px'}}>Title</h3>
-                <h4 style={{margin:'10px'}}>Title 2</h4>
-                </div>
-           
-        </button>
+       
     </div>
   )
 }
