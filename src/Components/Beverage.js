d7aaa7c768430cea8ada21b5045da142bd85b2d4
shrutiverma2208 <shrutiverma2208@gmail.com>
2024-02-07 21:52:06 +0530

    Complete beverage component

--- src/Components/Beverage.js
+++ src/Components/Beverage.js
@@ -3,12 +3,12 @@ import AddDeduct from './AddDeduct'
 
 const Beverage = ({onClick,beverage,price,image,subText}) => {
   return (
-    <>
-  <h1 style={{fontSize:'20px',marginTop:'25px'}}>Beverage</h1>
+    <div style={{alignItem:'center',justifyContent:'center'}}>
+  <h1 style={{fontSize:'20px',marginTop:'35px'}}>Beverage</h1>
 
     <div className='beverage-container'>
         <div className='beverage-tile' onClick={onClick}>
-          <h2 style={{fontSize:'17px'}}>{beverage}</h2>
+          <h2 style={{fontSize:'17px',marginTop:'20px'}}>{beverage}</h2>
           <h3 style={{fontSize:'12px',color:'grey'}}>{subText}</h3>
           <h3 style={{fontSize:'17px'}} className='price'>{price}</h3>
         </div>
@@ -19,7 +19,7 @@ const Beverage = ({onClick,beverage,price,image,subText}) => {
             </div>
         </div>
     </div>
-    </>
+    </div>
   )
 }
 
