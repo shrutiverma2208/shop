72c773fc4fa72869b25323e73860477a2a1a9b83
shrutiverma2208 <shrutiverma2208@gmail.com>
2024-02-07 14:54:30 +0530

    Adding counter functionality to AddDeduct Component

--- src/Components/AddDeduct.js
+++ src/Components/AddDeduct.js
@@ -2,27 +2,41 @@ import React from 'react'
 import {useState} from 'react';
 
 
+
 const AddDeduct = () => {
 
-  const[number,setNumber]=useState(0)
+  const[number,setNumber]=useState(1)
+  const[toggle,setToggle]=useState(true)
 
  
   const handleAdd=()=>{
     setNumber(number+1)
   }
   const handleDeduct=()=>{
-    if(number>0){
+    if(number>=0){
     setNumber(number-1)
   }
+  if(number==1){
+    setToggle(!toggle)
+  }
+  }
+   const isToggle=()=>{
+    setToggle(!toggle)
   }
 
 
   return (
+    <>
+    {toggle?<button style={{fontSize:'14px',padding:'7px 15px',borderRadius:'8px',color:'white',backgroundColor:'black'}} onClick={isToggle}>ADD +</button>:
     <div className='add-deduct'>
-      <button style={{fontSize:'20px',padding:'0px 3px'}} onClick={handleAdd}>+</button>
-     <h4 style={{fontSize:'20px',padding:'2px',fontWeight:'400'}}> {number} </h4> 
-      <button style={{fontSize:'20px',padding:'0px 5px'}} onClick={handleDeduct}>-</button>
-    </div>
+    <button style={{fontSize:'20px',padding:'0px 3px'}} onClick={handleDeduct}>-</button>
+   <h4 style={{fontSize:'20px',padding:'2px',fontWeight:'400'}}> {number} </h4> 
+    <button style={{fontSize:'20px',padding:'0px 5px'}} onClick={handleAdd}>+</button>
+  </div>
+  }
+    
+    </> 
+    
   )
 }
 
