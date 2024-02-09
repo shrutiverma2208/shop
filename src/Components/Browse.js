56460d7836f8f7e0f36299e2174e2ddf2abf309a
shrutiverma2208 <shrutiverma2208@gmail.com>
2024-02-09 19:11:43 +0530

    Complete Browser Component CSS

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -3,9 +3,10 @@ import React from 'react'
 const Browse = ({BrowserImage,BrowserName,onClick}) => {
   return (
     <div className='browser-container'>
+         <h3 style={{padding:'19px',fontSize:'19px'}}>Browse Categories</h3>
     <div className='browser-tile' onClick={onClick}>
-        <img style={{height:'18vh', width:'18vh', borderRadius:'110px',cursor:'pointer'}} src={BrowserImage}></img>
-        <div style={{fontSize:'18px',fontWeight:'500',padding:'5px'}}>{BrowserName}</div>
+        <img style={{height:'14vh', width:'14vh', borderRadius:'110px',cursor:'pointer'}} src={BrowserImage}></img>
+        <div style={{fontSize:'16px',fontWeight:'500',padding:'5px'}}>{BrowserName}</div>
     </div>
     </div>
   )
