19bb400ad73cdbfa2dbd435df54c2f763f491907
shrutiverma2208 <shrutiverma2208@gmail.com>
2024-02-09 21:27:36 +0530

    Complete Featured Component CSS

--- src/Components/Beverage.js
+++ src/Components/Beverage.js
@@ -3,14 +3,14 @@ import AddDeduct from './AddDeduct'
 
 const Beverage = ({onClick,beverage,price,image,subText}) => {
   return (
-    <div >
-  <h3 style={{fontSize:'19px',padding:'19px',width:'40%',overflow:'hidden'}}>Beverage</h3>
+    <div className='beverage'>
+  <h3 style={{fontSize:'19px',padding:'19px'}}>Beverage</h3>
 
     <div className='beverage-container'>
         <div className='beverage-tile' onClick={onClick}>
-          <h2 style={{fontSize:'17px',marginTop:'20px'}}>{beverage}</h2>
-          <h3 style={{fontSize:'12px',color:'grey'}}>{subText}</h3>
-          <h3 style={{fontSize:'17px'}} className='price'>{price}</h3>
+          <h2 style={{fontSize:'16px',marginTop:'20px'}}>{beverage}</h2>
+          <h3 style={{fontSize:'11px',color:'grey'}}>{subText}</h3>
+          <h3 style={{fontSize:'16px'}} className='price'>{price}</h3>
         </div>
         <div className='beverage-image'>
             <img src={image}></img>
--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -6,13 +6,13 @@ import buttonImg from './buttonImg.css';
 const Featured = ({featuredMainImage,title,onClick}) => {
   return (
     <div className='featured-container'>
-      <h3 style={{padding:'19px',fontSize:'19px'}}>Featured Merchants</h3>
+      <h3 style={{padding:'7px',fontSize:'16px'}}>Featured Merchants</h3>
         <div className='featured-image' onClick={onClick}>
         <img src={featuredMainImage} alt='main-img' style={{height:'23vh', width:'25vh', borderRadius:'22px'}}></img>
           
         </div>
         <div>
-            <h4 style={{margin:'10px',fontSize:'15px'}}>{title}</h4>
+            <h4 style={{margin:'10px',fontSize:'13px'}}>{title}</h4>
           </div>
     </div>
   )
