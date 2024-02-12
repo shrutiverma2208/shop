e5cfc4f3eb3c06abf94e29a52879e0585e1c068f
shrutiverma2208 <shrutiverma2208@gmail.com>
2024-02-12 13:31:54 +0530

    Bottom Navigation css

--- src/Components/BottomNavigation.js
+++ src/Components/BottomNavigation.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 const BottomNavigation = ({image1,title1,image2,title2,image3,title3,image4,title4,onClick1,onClick2,onClick3,onClick4}) => {
   return (
-    <div className='bottomNav-container'>
+ <div className='bottomNav-container'>
         <h3 style={{fontSize:'19px'}}>Bottom Navigation</h3>
-        <div className='bottomNav-tile'>
+    <div className='bottomNav-tile'>
+   
       <div style={{margin:'14px',cursor:'pointer'}} onClick={onClick1}>  
-        
        <img style={{height:'32px'}} src={image1} />
        <h3 style={{fontSize:'12px',fontWeight:'500'}}>{title1}</h3>
       </div>
+   
       <div style={{margin:'14px',cursor:'pointer'}} onClick={onClick2}>  
        <img style={{height:'32px'}} src={image2} />
        <h3  style={{fontSize:'12px',fontWeight:'500'}}>{title2}</h3>
@@ -21,10 +22,9 @@ const BottomNavigation = ({image1,title1,image2,title2,image3,title3,image4,titl
       <div style={{margin:'14px',cursor:'pointer'}} onClick={onClick4}>  
        <img style={{height:'32px'}} src={image4} />
        <h3  style={{fontSize:'12px',fontWeight:'500'}}>{title4}</h3>
-       
-      </div>
       </div>
     </div>
+</div>
   )
 }
 
